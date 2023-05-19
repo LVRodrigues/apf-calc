aaf8cd41c91800f95dbf933180d710dc775d7277
LVRodrigues <lvrodriguesline@gmail.com>
2023-05-19 15:13:12 -0300

    Parâmetros do componente App.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  title: string;
+  score: number;
+
+  constructor(app: AppComponent) {
+    this.title = app.title;
+    this.score = app.score;
+  }
 }
