8640203d031bcb77f074443864f102ed033c52ab
LVRodrigues <lvrodriguesline@gmail.com>
2023-09-05 12:29:54 -0300

    Ajustes nos tipos de Funções.

--- src/app/apf.service.ts
+++ src/app/apf.service.ts
@@ -11,5 +11,23 @@ export class ApfService {
 
     constructor() {
         this.project = new Project();
+        this.project.name = 'Auto Exemplo';
+        this.project.description = 'Exemplo criado automaticamente para teste';
+        this.project.responsible = 'Luciano Vieira Rodrigues';
+
+        let mod1 = new Module();
+        mod1.id = 1;
+        mod1.name = 'Primeiro'
+        this.project.modules.push(mod1);
+
+        let mod2 = new Module();
+        mod2.id = 2;
+        mod2.name = 'Segundo';
+        this.project.modules.push(mod2);
+
+        let mod3 = new Module();
+        mod3.id = 3;
+        mod3.name = 'Terceiro';
+        this.project.modules.push(mod3);
     }
 }
--- src/app/model/function.ts
+++ src/app/model/function.ts
@@ -1,16 +1,77 @@
 import { Data } from "./data";
 import { FunctionType } from "./function-type";
 
-export class Function {
+export abstract class Function {
     id!: number;
     name!: string;
     description: string | undefined;
-    type!: FunctionType;
-    datas!: Data[];
+    protected _type: FunctionType;
 
     constructor() {
         this.id = 0;
-        this.type = FunctionType.ALI;
+        this._type = FunctionType.ALI;
+    }
+
+    public get type() {
+        return this._type;
+    }
+};
+
+export abstract class FunctionData extends Function {
+    datas!: Data[];
+
+    constructor() {
+        super();
         this.datas = [];
+    }
+};
+
+export class FunctionALI extends FunctionData {
+
+    constructor() {
+        super();
+        this._type = FunctionType.ALI;
+    }
+}
+
+export class FunctionAIE extends FunctionData {
+
+    constructor() {
+        super();
+        this._type = FunctionType.AIE;
+    }
+}
+
+export abstract class FunctionTransaction extends Function {
+    datas!: FunctionData[]
+
+    constructor() {
+        super();
+        this.datas = [];
+    }
+}
+
+export class FunctionEE extends FunctionTransaction {
+
+    constructor() {
+        super();
+        this._type = FunctionType.EE;
+    }
+}
+
+export class FunctionCE extends FunctionTransaction {
+
+    constructor() {
+        super();
+        this._type = FunctionType.CE;
+    }
+}
+
+
+export class FunctionSE extends FunctionTransaction {
+
+    constructor() {
+        super();
+        this._type = FunctionType.SE;
     }
 }
