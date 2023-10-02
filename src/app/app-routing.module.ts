d2243058d4293bddc5bd7fc8f8b9fc0f260a9f54
LVRodrigues <lvrodriguesline@gmail.com>
2023-10-02 15:56:13 -0300

    Lista de funções.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { PropertiesComponent } from './properties/properties.component';
 import { ModulesComponent } from './modules/modules.component';
 import { ModuleGraphComponent } from './modules/module-graph/module-graph.component';
+import { FunctionsListComponent } from './modules/functions-list/functions-list.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, data: { title: 'Início' } },
     { path: 'properties', component: PropertiesComponent, data: { title: 'Propriedades' } },
     { path: 'modules', component: ModulesComponent, data: { title: 'Módulos' } },
-    { path: 'module-graph', component: ModuleGraphComponent, data: { title: 'Detalhes do Módulo'} }
+    { path: 'module-graph', component: ModuleGraphComponent, data: { title: 'Detalhes do Módulo'} },
+    { path: 'functions-list', component: FunctionsListComponent, data: { title: 'Lista de Funções'} },
 ];
 
 @NgModule({
