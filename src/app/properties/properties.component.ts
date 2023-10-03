1e5abc68df074385f56b537bdd796ad91b05b291
LVRodrigues <lvrodriguesline@gmail.com>
2023-10-03 15:12:40 -0300

    Propriedades do Projeto.

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../model/project';
 import { ApfService } from '../apf.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-properties',
@@ -9,9 +10,33 @@ import { ApfService } from '../apf.service';
 })
 export class PropertiesComponent {
 
-    @Input() project: Project;
+    project: Project;
 
-    constructor(private apf: ApfService) {
-        this.project = apf.project;
+    constructor(
+        private apf: ApfService,
+        private router: Router
+    ) {
+        this.project                = new Project();
+        this.project.name           = this.apf.project.name;
+        this.project.description    = this.apf.project.description;
+        this.project.date           = this.apf.project.date;
+        this.project.responsible    = this.apf.project.responsible;
+        this.project.score          = this.apf.project.score;
+        this.project.version        = this.apf.project.version;
+    }
+
+    confirm(): void {
+        this.apf.project.name           = this.project.name;
+        this.apf.project.description    = this.project.description;
+        this.apf.project.responsible    = this.project.responsible;
+        this.navigateBack();
+    }
+
+    cancel(): void {
+        this.navigateBack();
+    }
+
+    private navigateBack() {
+        this.router.navigate(['home']);
     }
 }
