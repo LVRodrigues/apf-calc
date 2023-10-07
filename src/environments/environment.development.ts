ada14c25eb5b0d4dc7f7fb7ab36b9f7968895992
LVRodrigues <lvrodriguesline@gmail.com>
2023-10-06 21:45:51 -0300

    Como publicar novas versões.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     appVersion: require('../../package.json').version + '-dev',
-    production: false
+    production: false,
+    releaseStamp: '2023-10-06T21:34:23-03:00'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     appVersion: require('../../package.json').version,
-    production: true
+    production: true,
+    releaseStamp: '2023-10-06T21:34:23-03:00'
 };
