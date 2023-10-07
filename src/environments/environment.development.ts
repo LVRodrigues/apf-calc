fd214c04bf5174780e151671bb44fa0ac62947d6
LVRodrigues <lvrodriguesline@gmail.com>
2023-10-06 21:47:11 -0300

    Versão 0.0.8

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     appVersion: require('../../package.json').version + '-dev',
     production: false,
-    releaseStamp: '2023-10-06T21:34:23-03:00'
+    releaseStamp: '2023-10-06T21:46:47-03:00'
 };
