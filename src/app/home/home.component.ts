9f95e95022fdff1a928d50986508288933fc85a5
LVRodrigues <lvrodriguesline@gmail.com>
2023-10-29 10:53:09 -0300

    Toolbox nos gráficos.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,58 +24,72 @@ export class HomeComponent {
         this.chartOptions = this.prepareChartOptions();
         this.data = [];
         this.timer = setInterval(() => {
-            this.data = [];
-            this.data.push({ name: 'Planejamento', value: 10 });
-            this.data.push({ name: 'Coordenação', value: 20 });
-            this.data.push({ name: 'Desenvolvimento', value: 70 });
-            this.data.push({ name: 'Testes', value: 50 });
-            this.data.push({ name: 'Implantação', value: 20 });
+            this.updateData();
 
+            let preferColor = '';
             let prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
             if (prefersDark) {
-                this.chartUpdate = {
-                    series: {
-                        data: this.data,
-                        label: {
-                            color: 'white'
-                        },
-                    },
-                    legend: {
-                        textStyle: {
-                            color: 'white',
-                        },
-                    },
-                };
+                preferColor = 'white';
             } else {
-                this.chartUpdate = {
-                    series: {
-                        data: this.data,
-                        label: {
-                            color: 'black'
-                        },
-                    },
-                    legend: {
-                        textStyle: {
-                            color: 'black',
-                        },
-                    },
-                };
+                preferColor = 'black';
             }
+            this.chartUpdate = {
+                series: {
+                    data: this.data,
+                    label: {
+                        color: preferColor,
+                    },
+                },
+                legend: {
+                    textStyle: {
+                        color: preferColor,
+                    },
+                },
+                toolbox: {
+                    iconStyle: {
+                        borderColor: preferColor,
+                    },
+                }
+            };
         }, 1000);
+    }
+    
+    private updateData(): void {
+        const score = this.apf.project.score;
+        this.data = [];
+        this.data.push({ name: 'Planejamento', value: score*0.1 });
+        this.data.push({ name: 'Coordenação', value: score*0.3 });
+        this.data.push({ name: 'Desenvolvimento', value: this.apf.project.score });
+        this.data.push({ name: 'Testes', value: score*0.4 });
+        this.data.push({ name: 'Implantação', value: score*0.1 });
     }
 
     prepareChartOptions(): EChartsOption {
         let result: EChartsOption = {
+            title: {
+                show: false,
+            },
             tooltip: {
                 trigger: 'item',
                 formatter: '{b}<br/>{c} {a} ({d}%)',
                 type: 'shadow',
             },
+            toolbox: {
+                show: true,
+                feature: {
+                    saveAsImage: {
+                        title: 'Salvar imagem'
+                    },
+                    restore: {
+                        title: 'Restaurar'
+                    },
+                },
+                bottom: '10'
+            },
             legend: {
                 align: 'auto',
-                bottom: 10,
             },
-            calculable: false,
+            center: ['50%', '50%'],
             series: [
                 {
                     name: 'Horas',
@@ -83,6 +97,8 @@ export class HomeComponent {
                     radius: [10, 50],
                     roseType: 'area',
                     data: this.data,
+                    width: 'auto' ,
+                    height: 'auto' ,
                 }],
         };
         return result;
