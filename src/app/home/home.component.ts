4e53b41541362420f0ca974af66a6e5c2df69fb4
LVRodrigues <lvrodriguesline@gmail.com>
2023-10-30 22:28:22 -0300

    Destaque das guias de paginação.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -41,6 +41,7 @@ export class HomeComponent {
                     },
                 },
                 legend: {
+                    show: true,
                     textStyle: {
                         color: preferColor,
                     },
